11745d818ba6d462dbe1cb60740c66f9f7cc008c
hyonreid015 <hyonreid015@gmail.com>
2020-02-28 23:14:40 -0800

    first unity

---
+++ Assets/Scripts/Rotato.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotato : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Destroy(other.gameObject);
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);  
+        }
+    }
+}
